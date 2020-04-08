a81acd928e8d9a82e3ed1d987b9ed66d0bd5fcb9
Matthew Hamm <mhamm@ucsc.edu>
2020-04-08 15:47:21 -0700

    Scenes & assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,13 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        console.log("menu")
+        // display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // launch the next scene
+        this.scene.start("playScene")
+    }
 }
